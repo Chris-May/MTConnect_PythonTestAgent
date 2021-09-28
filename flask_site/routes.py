41715f6179ed5d1af04d57bcf5d892d4ec9cb2ac
richardphi1618 <richardphi1618@gmail.com>
2021-09-27 21:56:43 -0400

    struggling with rendering xml

--- flask_site/routes.py
+++ flask_site/routes.py
@@ -1,24 +1,31 @@
-from flask_site import app
-from flask import Flask, render_template, url_for, flash, redirect, send_file
+from flask_site import app, api
+from flask import Flask, render_template, make_response
+from flask_restful import Resource, Api
 
+import lxml.html
+from lxml import etree
 
+def convert_xml_to_html(xsl, xml):
+    #this doesnt work -_-
+    xslt_doc = etree.parse(xsl)
+    xslt_transformer = etree.XSLT(xslt_doc)
+    source_doc = etree.parse(xml)
+    output_doc = xslt_transformer(source_doc)
+    output_doc.write("flask_site\\XML\\output-toc.html", pretty_print=True)
 
 @app.route("/")
 def home():
-    #return render_template ('Operator_Current_Example.xml')
     return render_template ('home.html')
 
 @app.route("/current")
 def current():
-    #return render_template ('Operator_Current_Example.xml')
-    return render_template ('current.html')
+    convert_xml_to_html('flask_site\\templates\\styles\\Streams.xsl', 'flask_site\\templates\\Operator_Current_Example.xml')
+    return render_template('current.html')
 
 @app.route("/sample")
 def sample():
-    #return render_template ('Operator_Current_Example.xml')
-    return render_template ('sample.html')
+    return render_template ('Operator_Current_Example.xml')
 
 @app.route("/probe")
 def probe():
-    #return render_template ('Operator_Current_Example.xml')
     return render_template ('probe.html')
